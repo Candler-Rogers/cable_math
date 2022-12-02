5087fb0ddca3562bf26186d9422df7fea0663e91
Candler Rogers <31564238+Candler-Rogers@users.noreply.github.com>
2022-12-02 14:26:17 -0800

    Create drop.py

---
+++ drop.py
@@ -0,0 +1,4 @@
+#implement Drop or maybe Cable class with attributes and methods
+#attributes: length, hi freq loss, low freq loss, tx gain, splitters,splitter loss, outlet count
+#inherit to RG 6 and Rg 11 classes with unique loss/gain values
+#create an instance for each new question
