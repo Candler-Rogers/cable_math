5eff1a967812bf59a72cee3fee8e4a12a07f8371
Candler Rogers <31564238+Candler-Rogers@users.noreply.github.com>
2023-01-18 08:58:21 -0800

    Update drop.py
    Added the Drop class

--- drop.py
+++ drop.py
@@ -2,3 +2,12 @@
 #attributes: length, hi freq loss, low freq loss, tx gain, splitters,splitter loss, outlet count
 #inherit to RG 6 and Rg 11 classes with unique loss/gain values
 #create an instance for each new question
+
+class Drop ):
+    def __init__(self, length, cable_type, splitter, outlets):
+        #attributes
+        self.length = length
+        self.cable_type = cable_type
+        #splitter expects boolean
+        self.splitters = splitter
+        self.outlets = outlets
